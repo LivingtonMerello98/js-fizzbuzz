4357dc67210a4a79bb5ca04395e200bbff0b9afa
LivingtonMerello98 <livington.merello@gmail.com>
2024-04-30 16:02:17 +0200

    [upd] iteration 1 to 100

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 'use strict'
 
+// Iterazione da 1 a 100
+for(let i = 0; i <= 100; i++){}
+// Sostituizione multipli di 3 con "Fizz"
+// Sostituzione multipli di 5 con "Buzz"
+// Sostituzione multipli di 3 e 5 con "FizzBuzz"
+// Altrimenti stampare il numero
