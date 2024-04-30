e405d0986dfaffde95d6c5d28670fbb7c8439b70
LivingtonMerello98 <livington.merello@gmail.com>
2024-04-30 16:27:52 +0200

    [upd] replacement  multiples of 3 and 5, otherwise print the "i"

--- main.js
+++ main.js
@@ -8,11 +8,13 @@ for(let i = 1; i <= 100; i++){
         console.log("Fizz");
     // Sostituzione multipli di 5 con "Buzz"
     }else if(i % 5 === 0){
+    // Sostituzione multipli di 3 e 5 con "FizzBuzz"
         console.log("Buzz");
+    }else if(i % 3 === 0 || i % 5 === 0) {
+        console.log("FizzBuzz");
     }else{
+    // Altrimenti stampare il numero
         console.log(i);
     }
 }
-// Sostituzione multipli di 5 con "Buzz"
-// Sostituzione multipli di 3 e 5 con "FizzBuzz"
 // Altrimenti stampare il numero
