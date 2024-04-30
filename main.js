072a355043b9a1ef21a2e92dfcd18c2c40124ba0
LivingtonMerello98 <livington.merello@gmail.com>
2024-04-30 16:18:46 +0200

    [upd] replacement of multiples 3 with Fizz

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 'use strict'
 
+console.log("sono correttamente collegato al doc.html")
 // Iterazione da 1 a 100
-for(let i = 0; i <= 100; i++){}
+for(let i = 0; i <= 100; i++){
+    // Sostituizione multipli di 3 con "Fizz"
+    if(i % 3 === 0){
+        console.log("Fizz");
+    }else{
+        console.log(i);
+    }
+}
 // Sostituizione multipli di 3 con "Fizz"
 // Sostituzione multipli di 5 con "Buzz"
 // Sostituzione multipli di 3 e 5 con "FizzBuzz"
