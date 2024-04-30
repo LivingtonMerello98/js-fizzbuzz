bc1acd534def29c37c14d17f79dc4ff842345ea8
LivingtonMerello98 <livington.merello@gmail.com>
2024-04-30 16:22:05 +0200

    [upd] replacement of multiples of 5 with Buzz

--- main.js
+++ main.js
@@ -2,15 +2,17 @@
 
 console.log("sono correttamente collegato al doc.html")
 // Iterazione da 1 a 100
-for(let i = 0; i <= 100; i++){
+for(let i = 1; i <= 100; i++){
     // Sostituizione multipli di 3 con "Fizz"
     if(i % 3 === 0){
         console.log("Fizz");
+    // Sostituzione multipli di 5 con "Buzz"
+    }else if(i % 5 === 0){
+        console.log("Buzz");
     }else{
         console.log(i);
     }
 }
-// Sostituizione multipli di 3 con "Fizz"
 // Sostituzione multipli di 5 con "Buzz"
 // Sostituzione multipli di 3 e 5 con "FizzBuzz"
 // Altrimenti stampare il numero
