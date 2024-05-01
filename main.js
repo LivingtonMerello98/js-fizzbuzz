23d391a4dc8572fa1961c016498d77a4fcbd3930
LivingtonMerello98 <livington.merello@gmail.com>
2024-05-02 00:30:13 +0200

    [upd] Bonus1 // element in html created

--- main.js
+++ main.js
@@ -2,18 +2,37 @@
 
 console.log("sono correttamente collegato al doc.html")
 // Iterazione da 1 a 100
-for(let i = 1; i <= 100; i++){
-    // Sostituizione multipli di 3 con "Fizz"
-    if(i % 3 === 0){
-        console.log("Fizz");
-    // Sostituzione multipli di 5 con "Buzz"
-    }else if(i % 5 === 0){
-    // Sostituzione multipli di 3 e 5 con "FizzBuzz"
-        console.log("Buzz");
-    }else if(i % 3 === 0 || i % 5 === 0) {
-        console.log("FizzBuzz");
-    }else{
-    // Altrimenti stampare il numero
-        console.log(i);
+
+//bonus1
+function fizzBuzz(){
+    const container = document.getElementById('container');
+
+
+    for(let i = 1; i <= 100; i++){
+        //creo il div
+        const element = document.createElement('div');
+        //chiamo le sue classi e aggiungo la classe item
+        element.classList.add("item");
+
+        // Sostituizione multipli di 3 con "Fizz"
+        if(i % 3 === 0){
+            console.log("Fizz");
+        // Sostituzione multipli di 5 con "Buzz"
+        }else if(i % 5 === 0){
+        // Sostituzione multipli di 3 e 5 con "FizzBuzz"
+            console.log("Buzz");
+        }else if(i % 3 === 0 || i % 5 === 0) {
+            console.log("FizzBuzz");
+        }else{
+        // Altrimenti stampare il numero
+            console.log(i);
+        }
+        container.appendChild(element);
     }
+
 }
+//richiamo la funzione
+fizzBuzz();
+
+
+
