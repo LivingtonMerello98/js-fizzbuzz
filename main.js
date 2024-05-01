f83e92f46f6d510a9c99fa673f7218c3958e10ee
LivingtonMerello98 <livington.merello@gmail.com>
2024-05-02 01:45:41 +0200

    [upd] fizz, buzz and fizzBuzz bg-colors and color classes in style.css

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ console.log("sono correttamente collegato al doc.html")
 //bonus2
 const title = document.getElementById('title');
 title.classList.add( 'text-center','fs-32','py-30');
-title.textContent="FizzBuzz";
+title.style.color='#f48167';
+title.textContent="FizzBuzzDOM";
 
 
 //bonus1
@@ -24,18 +25,22 @@ function fizzBuzz(){
         console.log('fizzBuzz');
         //aggiunta parola Fizz al div
         element.textContent =  'fizzbuzz';
+        element.classList.add('fizzBuzz','text-white');
     // Sostituizione multipli di 3 con "Fizz"    
     } else if(i % 3 === 0){
         console.log('Fizz');
         element.textContent =  'Fizz';
+        element.classList.add('fizz','text-white');
     // Sostituzione multipli di 5 con "Buzz"
     } else if(i % 5 === 0){
         console.log('Buzz')
-        element.textContent =  'Buzz'
+        element.textContent =  'Buzz';
+        element.classList.add('buzz','text-white');
     } else {
     // Altrimenti stampare il numero
         console.log(i);
         element.textContent = i;
+        element.classList.add('number');
     }
         container.appendChild(element)
     }
